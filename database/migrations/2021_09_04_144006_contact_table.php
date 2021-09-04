310e16b1006cf21fadffa6ca794cf356054244cb
D41152670 <Thiago.Mendes@adtalem.com>
2021-09-04 12:05:09 -0300

    Initial commit

---
+++ database/migrations/2021_09_04_144006_contact_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->char('date_birth', 10);
+            $table->char('phone', 14);
+            $table->string('address', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_info');
+    }
+}
